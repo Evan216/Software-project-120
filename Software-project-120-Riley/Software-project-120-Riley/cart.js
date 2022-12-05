17ec0b73f231eb154c40d71faac3910c0aee1ed6
jaz <jazlynntroop@gmail.com>
2022-12-05 09:23:20 -0500

    add payment options

--- Software-project-120-Riley/Software-project-120-Riley/cart.js
+++ Software-project-120-Riley/Software-project-120-Riley/cart.js
@@ -101,3 +101,52 @@ function updateCartTotal(){
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
+
+let users = [
+    {
+        username: "Evan",
+        password: "1234"
+        },
+    {
+        username: "riley",
+        password: "4321"
+    }    
+]
+
+// login function
+function login() {
+    // retrieve username and password from form
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  if (localStorage.getItem(username) === password) {
+    alert('Login successful');
+
+  }
+    else {
+      alert('Login failed');
+    
+  }
+  
+}
+
+// function to create a new user with name, email, password
+function register() {
+    // store new user data 
+  let registerUser = document.getElementById("newUserName").value;
+    // store new user password
+  let registerPassword = document.getElementById("newPassword").value;
+
+  if (localStorage.getItem(registerUser) !== null) {
+    alert("Username already exists");}
+
+  localStorage.setItem(registerUser, registerPassword);
+  alert("Registration successful");
+
+users.push({registerUser, registerPassword});
+}
+
+// function to alert login needed for access to page
+function feature(){
+    alert('please login to use this feature!!')
+}
