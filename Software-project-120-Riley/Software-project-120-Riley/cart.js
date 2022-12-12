acf8c8c19a8f9575d022e7e81dc10cd421eb2399
jaz <jazlynntroop@gmail.com>
2022-12-12 16:04:53 -0500

    receipt css

--- Software-project-120-Riley/Software-project-120-Riley/cart.js
+++ Software-project-120-Riley/Software-project-120-Riley/cart.js
@@ -151,15 +151,3 @@ function feature(){
     alert('please login to use this feature!!')
 }
 
-//function to add recomended 
-function addItemToCart(title, price, imageSrc){
-    var recRow = document.createElement('div')
-    recRow.classList.add('rec-row')
-    var recItems = document.getElementsByClassName('rec-items')[0]
-    var recItemNames = recItems.getElementsByClassName('rec-item-title')
-    for(var i = 0; i < recItemNames.length; i++){
-        if (recItemNames[i].innerText == title) {
-            alert('This item is already added!!')
-            return
-        }
-    }
