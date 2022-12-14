dc7053a28fa5206be54a49c0d8485816e8102ef3
jaz <jazlynntroop@gmail.com>
2022-12-14 10:21:02 -0500

    updated and corrected add.js

--- Menu.js
+++ Menu.js
@@ -1,26 +1,138 @@
-var menutems = JSON.parse(localStorage.getItem('foodItems'))
+// localStorage.setItem("lastname", "Smith");
+// // Retrieve
+// document.getElementById("demo").innerHTML = localStorage.getItem("lastname");
 
-function renderManagerMenu(<div class="">
-<img class="app" src="${menuItem.image}" alt="">
-<div class="title">${menuItem.name}</div><div class="price"> ${menuItem.price}</div>
-<button><a class="add" href="">Add Item</a></button>
-</div>) {
 
-    menulist.innerHTML = "";
-    foodItems.forEach((foodItem) => {
-        menulist.innerHTML += 
-            <div class="menulist">
-                <div class="categories">
-                    <div class="shop-item">
-                        <ion-icon class="removeButton" onclick="removeFromMenu(${foodItem.id})" name="close-circle-outline">Remove</ion-icon>
-                        <img class="shop-item-image" src="${foodItem.image}" alt="">
-                        <h3 class="itemName">${foodItem.name}</h3>
-                        <div class="shop-item-details">
-                        <span class="shop-item-price">${foodItem.price}</span> <br> 
-                        <button class="btn btn-primary shop-item-button" type="button">Item back to cart</button>
-                        </div>
-                    </div>
-            <div>
-        ;
-    });
-};
+
+
+// var menutems = JSON.parse(localStorage.getItem('foodItems'))
+
+// function renderManagerMenu(<div class="">
+// <img class="app" src="${menuItem.image}" alt="">
+// <div class="title">${menuItem.name}</div><div class="price"> ${menuItem.price}</div>
+// <button><a class="add" href="">Add Item</a></button>
+// </div>) {
+
+//     menulist.innerHTML = "";
+//     foodItems.forEach((foodItem) => {
+//         menulist.innerHTML += 
+//             <div class="menulist">
+//                 <div class="categories">
+//                     <div class="shop-item">
+//                         <ion-icon class="removeButton" onclick="removeFromMenu(${foodItem.id})" name="close-circle-outline">Remove</ion-icon>
+//                         <img class="shop-item-image" src="${foodItem.image}" alt="">
+//                         <h3 class="itemName">${foodItem.name}</h3>
+//                         <div class="shop-item-details">
+//                         <span class="shop-item-price">${foodItem.price}</span> <br> 
+//                         <button class="btn btn-primary shop-item-button" type="button">Item back to cart</button>
+//                         </div>
+//                     </div>
+//             <div>
+        
+//     });
+// };
+
+
+
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase!')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('Whoops! This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+    <div class="">
+        <img class="app" src="${menuItem.image}" alt="">
+        <p>Evil Spinach Artichoke Dip $10.30</p>
+        <div class="title">${menuItem.name}</div><div class"price"> ${menuItem.price}</div>
+        <button><a class="add" href="">Add Item</a></button>
+    </div>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
--- add.js
+++ add.js
@@ -3,47 +3,46 @@ var appetizers = [
         id: 0,
         image: "menuimg/calamari-dpv-590x365.webp",        
         name: "Evil Calamri",
-        price: $11.99,
+        price: "11.99",
     },
     {
         id: 1,
         image: "menuimg/Lasagna-Fritta-gv-590x365.webp",        
         name: "Evil Lasagna Fritta",
-        price: $10.50,
+        price: "10.50",
     },
     {
         id: 2,
         image: "menuimg/Spinach-Artichoke-Dip-gv-590x365.webp",        
         name: "Evil Spinach Artichoke Dip",
-        price: $10.30,
+        price: "10.30",
 
     },
     {
         id: 3,
         image: "menuimg/toasted-ravioli-gv-590x365.webp",        
         name: "Evil Toasted Ravioli",
-        price: $7.99,
+        price: "7.99",
     },
     {
         id: 4,
         image: "menuimg/soup-array-dpv-590x365.webp",       
         name: "Evil Soup",
-        price: $7.99,
-
-
+        price: "7.99",
     },
     {
         id: 5,
-        image: "bandidos_images/steak_tacos.jpg",
-        name: "Evil Steak Tacos",
-        price: $7.99,
+        image: "menuimg/toasted-ravioli-gv-590x365.webp",
+        name: "Evil Toasted Raviloli",
+        price: "7.99",
 
     },
     {
         id: 6,
+        image: "menuimg/Dipping-Sauces-dvp-1180X730.jpg",          
         name: "Evi Dipping Sauces",
-        price: $11.99,
-        image: "menuimg/Dipping-Sauces-dvp-1180X730.jpg",        
+        price: "11.99",
+      
     }
 ]
     var entrees = [
@@ -51,98 +50,98 @@ var appetizers = [
             id: 7,
             image: "menuimg/Five-Cheese-Ziti-Lunch-gv-590x365.webp",        
             name: "Evil Cheese Ziti",
-            price: $11.99,
+            price: "11.99",
         },
         {
             id: 8,
-            image: "menuimg/Ravioli-Carbonara-gv-590x365.webp"        
+            image: "menuimg/Ravioli-Carbonara-gv-590x365.webp",        
             name: "Evil Ravoli Carbanara",
-            price: $14.99,
+            price: "14.99",
         },
         {
             id: 9,
             image: "menuimg/HerbGrilledSalmon_v1_gv.webp" ,        
             name: "Evil Grilled Salmon",
-            price: $10.30,
+            price: "10.30",
     
         },
         {
             id: 10,
             image:"menuimg/Spaghetti-meatballs-gv-590x365.webp",        
             name: "Evil Spaghetti meatballs",
-            price: $13.99,
+            price: "13.99",
         },
         {
             id: 11,
             image: "menuimg/Merch_Steak_Alfredo_Salad_Wine_GV.webp",       
             name: "Evil Steak Alfredo",
-            price: $13.99,
+            price: "13.99",
     
         },
         {
             id: 12,
             image: "menuimg/Ravioli-Carbonara-gv-590x365.webp",
             name: "Evil Ravioli Carbonara",
-            price: $11.99,
+            price: "11.99",
     
         },
         {
             id: 13,
             image: "menuimg/seafood-alfredo-gv-590x365.webp",            
-            name: "Evi Seafood Alfredo",
-            price: $11.99,
+            name: "Evil Seafood Alfredo",
+            price: "11.99",
         },
 
         {
             id: 14,
             image: "menuimg/dinner-chicken-parm-dpv-590x365.webp",
             name: "Evil Chicken Parmesean",
-            price: $11.99,
+            price: "11.99",
     
         },
         {
             id: 15,
             image:"menuimg/Shrimp-Scampi-Dinner-gv-590x365.webp" "menuimg/dinner-chicken-parm-dpv-590x365.webp",
             name: "Evil Shromp Scampi",
-            price: $12.99,
+            price: "12.99",
     
         },
         {
             id: 16,
             image: "menuimg/chicken-and-shrimp-carbonara-dpv-590x365.webp",
             name: "Evil Chicken and Shrimp Carbonara",
-            price: $11.99,
+            price: "11.99",
     
         }
     ]
 
-    var desset = [
+    var dessert = [
         {
             id: 17,
             image: "menuimg/black-tie-mousse-cake-dpv-590x365.webp",
-            name: "Evil Vlack Tie Mousse",
-            price: $11.99,
+            name: "Evil Black Tie Mousse",
+            price: "11.99",
     
         },
         {
             id: 18,
             image: "menuimg/Strawberry-Cheesecake-gv-590x365.webp"            
             name: "Evi Strawberry Cheesecake",
-            price: $4.99,
+            price: "4.99",
         },
 
         {
             id: 19,
             image: "menuimg/Strawberry-Cream-Cake-Updated-gv-590x365.jpg",
             name: "Evil Strawberry Cream Cake",
-            price: $4.50,
+            price: "4.50",
     
         },
         {
             id: 20,
             image:"menuimg/Tiramisu-gv-590x365.webp",
             name: "Evil Tiramisu",
-            price: $3.99,
+            price: "3.99",
     
         }
     ]
