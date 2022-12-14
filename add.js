2213fea60a00d8b2d2013cc681cc0082e524a834
jaz <jazlynntroop@gmail.com>
2022-12-14 01:21:28 -0500

    add.js full js menu

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-var menuItems = [
+var appetizers = [
     {
         id: 0,
         image: "menuimg/calamari-dpv-590x365.webp",        
@@ -42,14 +42,107 @@ var menuItems = [
     {
         id: 6,
         name: "Evi Dipping Sauces",
-        price: 11.99,
-        image: "menuimg/Dipping-Sauces-dvp-1180X730.jpg",
-
+        price: $11.99,
+        image: "menuimg/Dipping-Sauces-dvp-1180X730.jpg",        
     }
 ]
-    // {
-    //     id: 7,
-    //     name: "Shrimp Tacos",
-    //     price: 11.99,
-    //     image: "menuimg/Five-Cheese-Ziti-Lunch-gv-590x365.webp",
-    // },
+    var entrees = [
+        {
+            id: 7,
+            image: "menuimg/Five-Cheese-Ziti-Lunch-gv-590x365.webp",        
+            name: "Evil Cheese Ziti",
+            price: $11.99,
+        },
+        {
+            id: 8,
+            image: "menuimg/Ravioli-Carbonara-gv-590x365.webp"        
+            name: "Evil Ravoli Carbanara",
+            price: $14.99,
+        },
+        {
+            id: 9,
+            image: "menuimg/HerbGrilledSalmon_v1_gv.webp" ,        
+            name: "Evil Grilled Salmon",
+            price: $10.30,
+    
+        },
+        {
+            id: 10,
+            image:"menuimg/Spaghetti-meatballs-gv-590x365.webp",        
+            name: "Evil Spaghetti meatballs",
+            price: $13.99,
+        },
+        {
+            id: 11,
+            image: "menuimg/Merch_Steak_Alfredo_Salad_Wine_GV.webp",       
+            name: "Evil Steak Alfredo",
+            price: $13.99,
+    
+        },
+        {
+            id: 12,
+            image: "menuimg/Ravioli-Carbonara-gv-590x365.webp",
+            name: "Evil Ravioli Carbonara",
+            price: $11.99,
+    
+        },
+        {
+            id: 13,
+            image: "menuimg/seafood-alfredo-gv-590x365.webp",            
+            name: "Evi Seafood Alfredo",
+            price: $11.99,
+        },
+
+        {
+            id: 14,
+            image: "menuimg/dinner-chicken-parm-dpv-590x365.webp",
+            name: "Evil Chicken Parmesean",
+            price: $11.99,
+    
+        },
+        {
+            id: 15,
+            image:"menuimg/Shrimp-Scampi-Dinner-gv-590x365.webp" "menuimg/dinner-chicken-parm-dpv-590x365.webp",
+            name: "Evil Shromp Scampi",
+            price: $12.99,
+    
+        },
+        {
+            id: 16,
+            image: "menuimg/chicken-and-shrimp-carbonara-dpv-590x365.webp",
+            name: "Evil Chicken and Shrimp Carbonara",
+            price: $11.99,
+    
+        }
+    ]
+
+    var desset = [
+        {
+            id: 17,
+            image: "menuimg/black-tie-mousse-cake-dpv-590x365.webp",
+            name: "Evil Vlack Tie Mousse",
+            price: $11.99,
+    
+        },
+        {
+            id: 18,
+            image: "menuimg/Strawberry-Cheesecake-gv-590x365.webp"            
+            name: "Evi Strawberry Cheesecake",
+            price: $4.99,
+        },
+
+        {
+            id: 19,
+            image: "menuimg/Strawberry-Cream-Cake-Updated-gv-590x365.jpg",
+            name: "Evil Strawberry Cream Cake",
+            price: $4.50,
+    
+        },
+        {
+            id: 20,
+            image:"menuimg/Tiramisu-gv-590x365.webp",
+            name: "Evil Tiramisu",
+            price: $3.99,
+    
+        }
+    ]
